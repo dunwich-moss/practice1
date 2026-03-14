1b19102dd5df3ee0629acf7359902478a78a0454
dunwich-moss <krivulina160309@gmail.com>
2026-03-14 14:20:52 +0300

    ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    int a, b;
+    printf("Введите два числа: ");
+    scanf("%d %d", &a, &b);
+    printf("Сумма: %d", a + b);
+    printf("Разность: %d", a - b);
+    printf("Произведение: %d", a * b);
+    printf("Целочисленное деление: %d", a / b);
+    return 0;
+}
